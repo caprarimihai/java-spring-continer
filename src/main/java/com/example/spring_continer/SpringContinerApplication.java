985ac3fa086da83ee87d828798ba12d333d894ce
mihai caprari <caprarimihail@gmail.com>
2025-02-22 10:54:46 +0200

    first commit

---
+++ src/main/java/com/example/spring_continer/SpringContinerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_continer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_continer/SpringContinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_continer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
